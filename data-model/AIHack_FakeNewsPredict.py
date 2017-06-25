4220e5a08fe6536ea11c32c7b390a8a20a9838b3
Linda Ristevski <lindaristevski@Lindas-MacBook-Pro.local>
2017-06-24 20:34:27 -0400

    Data Model

---
+++ data-model/AIHack_FakeNewsPredict.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.externals import joblib
+
+file = open('../web-scrape/buffer.txt', 'r')
+data = file.read()
+
+data_arr = []
+data_arr.append(data)
+
+lr = joblib.load('AIHack_DataModel.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
+transformer = joblib.load('transformer.pkl')
+
+X_ka = vectorizer.transform(data_arr)
+tfidf_ka = transformer.transform(X_ka)
+
+lr_predict = lr.predict(tfidf_ka)
+print(lr_predict)
